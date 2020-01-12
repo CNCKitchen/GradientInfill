496fdf03e8c2a0e3cb3fe361158f246e56e67cff
Ricardo Band <email@ricardo.band>
2020-01-12 18:15:10 +0100

    just did a little polishing
    always add a shebang line to your executable scripts so your operating system knows how to execute your script. Then instead of typing `python3 myscript.py` you can make it executable with `chmod +x myscript.py` and execute it simply with `./myscript.py`.
    
    In a python script you should have 2 empty lines in the top of your hierarchy inside a class or function one empty line is fine.
    
    If you want to document your function and classes use multi-line strings in the first line after the definition and called docstrings. These are usually structured with a one line explanation of your function, followed by a longer explanation and then a formatted description of the functions parameters and return values. There are multiple standards to do that. Just choose one of them and try to stick to it. I personally like the sphinx style.
    
    You have a lot of one line functions. I would rather write that one-liner directly instead of moving it away as a function. I'm not sure if if your also gain some performance with that but while reading the code I don't have to jump between a lot of functions to understand what it does.
    
    Most of my edits are not my own taste but rather rules from python itself. Have a look at the PEP-8 document which describes all of that in detail. There is also a nice tool to check your code called flake8.

--- addGradientInfill.py
+++ addGradientInfill.py
@@ -1,49 +1,55 @@
-##################################################
-## Gradient Infill for 3D prints
-##################################################
-## MIT license
-##################################################
-## Author: Stefan Hermann - CNC Kitchen
-## Version: 1.0
-##################################################
+#!/usr/bin/env python3
+"""
+Gradient Infill for 3D prints
+
+License: MIT
+Author: Stefan Hermann - CNC Kitchen
+Version: 1.0
+"""
 
 import re
-from collections import namedtuple
 from enum import Enum
+from collections import namedtuple
+
 
 class InfillType(Enum):
     SMALL_SEGMENTS = 1  # infill with small segments like honeycomb or gyroid
-    LINEAR = 2  # linear infill like rectilinear or triangles
+    LINEAR = 2          # linear infill like rectilinear or triangles
 
+    
 Point2D = namedtuple('Point2D', 'x y')
 Segment = namedtuple('Segment', 'point1 point2')
 
-################ EDIT this section for your creation parameters
+# EDIT this section for your creation parameters
 
 INPUT_FILE_NAME = "cloverleaf_wHole_gyroid.gcode"
 OUTPUT_FILE_NAME = "BOWDEN_cloverleaf_wHole_gyroid.gcode"
 
 infillType = InfillType.SMALL_SEGMENTS
 
-maxFlow = 350              # maximum extrusion flow
-minFlow = 50               # minimum extrusion flow
-gradientThickness = 6      # thickness of the gradient (max to min) in mm
-gradientDiscretization = 4 # only applicable for linear infills; number of segments within the 
-                           # gradient( segmentLength=gradientThickness/gradientDiscretization); 
-                           # use sensible values to not overload the printer
+maxFlow = 350               # maximum extrusion flow
+minFlow = 50                # minimum extrusion flow
+gradientThickness = 6       # thickness of the gradient (max to min) in mm
+gradientDiscretization = 4  # only applicable for linear infills; number of segments within the 
+                            # gradient( segmentLength=gradientThickness/gradientDiscretization);
+                            # use sensible values to not overload the printer
 
-###############################################################
+# End edit
+
 
 class Section(Enum):
     NOTHING = 0
     INNER_WALL = 1
     INFILL = 2
 
+
 def dist(segment, point):
-    # Calculate the distance from a point to a line with finite length
+    """
+    Calculate the distance from a point to a line with finite length
+    """
     px = segment.point2.x - segment.point1.x
     py = segment.point2.y - segment.point1.y
-    norm = px*px + py*py
+    norm = px * px + py * py
     u = ((point.x - segment.point1.x) * px + (point.y - segment.point1.y) * py) / float(norm)
     if u > 1:
         u = 1
@@ -53,44 +59,61 @@ def dist(segment, point):
     y = segment.point1.y + u * py
     dx = x - point.x
     dy = y - point.y
-    return (dx*dx + dy*dy) ** .5
+
+    return (dx * dx + dy * dy) ** .5
+
 
 def get_points_distance(point1, point2):
-    return ((point1.x - point2.x)**2 + (point1.y - point2.y)**2)**0.5
+    return ((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2) ** 0.5
 
 
 def min_distance_from_segment(segment, segments):
     middlePoint = Point2D((segment.point1.x + segment.point2.x) / 2, (segment.point1.y + segment.point2.y) / 2)
+
     return min(dist(s, middlePoint) for s in segments)
 
+
 def getXY(currentLine):
-    # Returns the X and Y value of the current line
+    """
+    Returns the X and Y value of the current line
+    """
     elementX = re.search(r"X(\d*\.?\d*)", currentLine).group(1)
     elementY = re.search(r"Y(\d*\.?\d*)", currentLine).group(1)
+
     return Point2D(float(elementX), float(elementY))
 
-def mapRange( a, b, s):
+
+def mapRange(a, b, s):
     (a1, a2), (b1, b2) = a, b
-    return  b1 + ((s - a1) * (b2 - b1) / (a2 - a1))
+
+    return b1 + ((s - a1) * (b2 - b1) / (a2 - a1))
+
 
 def get_extrusion_command(x, y, extrusion):
+    # with python 3.6+ you can write that as a format string:
+    # return f"G1 X{ round(x, 3) } Y{ round(y, 3) } E{ round(extrusion, 5) }\n"
     return "G1 X{} Y{} E{}\n".format(round(x, 3), round(y, 3), round(extrusion, 5))
 
 
 def is_begin_layer_line(line):
     return line.startswith(";LAYER:")
 
+
 def is_begin_inner_wall_line(line):
     return line.startswith(";TYPE:WALL-INNER")
+
 
 def is_end_inner_wall_line(line):
     return line.startswith(";TYPE:WALL-OUTER")
 
+
 def is_extrusion_line(line):
     return "G1" in line and " X" in line and "Y" in line and "E" in line
 
+
 def is_begin_infill_segment_line(line):
     return line.startswith(";TYPE:FILL")
+
 
 def main():
     currentSection = Section.NOTHING
@@ -119,13 +142,15 @@ def main():
 
             if currentSection == Section.INFILL:
                 if "F" in currentLine and "G1" in currentLine:
+                    # python3.6+ f-string variant:
+                    # outputFile.write("G1 F{ re.search(r"F(\d*\.?\d*)", currentLine).group(1)) }\n"
                     outputFile.write("G1 F{}\n".format(re.search(r"F(\d*\.?\d*)", currentLine).group(1)))
                 if "E" in currentLine and "G1" in currentLine and " X" in currentLine and "Y" in currentLine:
                     currentPosition = getXY(currentLine)
+                    splitLine = currentLine.split(" ")
 
                     if infillType == InfillType.LINEAR:
-                        #find extrusion length
-                        splitLine = currentLine.split(" ")
+                        # find extrusion length
                         for element in splitLine:
                             if "E" in element:
                                 extrusionLength = float(element[1:len(element)])
@@ -148,7 +173,7 @@ def main():
                                 if shortestDistance < gradientThickness:
                                     segmentExtrusion = extrusionLengthPerSegment * mapRange(
                                         (0, gradientThickness),
-                                        (maxFlow/100, minFlow/100),
+                                        (maxFlow / 100, minFlow / 100),
                                         shortestDistance
                                     )
                                 else:
@@ -157,7 +182,7 @@ def main():
                                 outputFile.write(get_extrusion_command(segmentEnd.x, segmentEnd.y, segmentExtrusion))
 
                                 lastPosition = segmentEnd
-                            #MissingSegment
+                            # MissingSegment
                             segmentLengthRatio = get_points_distance(lastPosition, currentPosition) / segmentLength
 
                             outputFile.write(get_extrusion_command(
@@ -166,11 +191,10 @@ def main():
                                 segmentLengthRatio * extrusionLength * maxFlow / 100
                             ))
                         else:
-                            splitLine = currentLine.split(" ")
                             outPutLine = ""
                             for element in splitLine:
                                 if "E" in element:
-                                    outPutLine = outPutLine + "E" + str(round(extrusionLength * maxFlow/100,5))
+                                    outPutLine = outPutLine + "E" + str(round(extrusionLength * maxFlow / 100, 5))
                                 else:
                                     outPutLine = outPutLine + element + " "
                             outPutLine = outPutLine + "\n"
@@ -180,16 +204,16 @@ def main():
                     # gyroid or honeycomb
                     if infillType == InfillType.SMALL_SEGMENTS:
                         shortestDistance = min_distance_from_segment(
-                            Segment(lastPosition, currentPosition), perimeterSegments
+                            Segment(lastPosition, currentPosition),
+                            perimeterSegments
                         )
 
                         outPutLine = ""
                         if shortestDistance < gradientThickness:
-                            splitLine = currentLine.split(" ")
                             for element in splitLine:
                                 if "E" in element:
-                                    newE = float(element[1:len(element)]) * mapRange((0, gradientThickness), (maxFlow/100, minFlow/100), shortestDistance)
-                                    outPutLine = outPutLine + "E" + str(round(newE,5))
+                                    newE = float(element[1:len(element)]) * mapRange((0, gradientThickness), (maxFlow / 100, minFlow / 100), shortestDistance)
+                                    outPutLine = outPutLine + "E" + str(round(newE, 5))
                                 else:
                                     outPutLine = outPutLine + element + " "
                             outPutLine = outPutLine + "\n"
@@ -198,14 +222,14 @@ def main():
                 if ";" in currentLine:
                     currentSection = Section.NOTHING
 
-
-            #line with move
+            # line with move
             if " X" in currentLine and " Y" in currentLine and ("G1" in currentLine or "G0" in currentLine):
                 lastPosition = getXY(currentLine)
 
-            #write uneditedLine
+            # write uneditedLine
             if writtenToFile == 0:
                 outputFile.write(currentLine)
+
 
 if __name__ == '__main__':
     main()
